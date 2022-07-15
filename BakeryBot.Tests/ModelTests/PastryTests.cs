0c3f458a4769f73cb2558aa161f25a9249d2be3e
Kyle Chinick <kylechinick@icloud.com>
2022-07-15 16:22:56 -0700

    Add support for GREEN PastryCost_ReturnBaseCost_Int() test

--- BakeryBot.Tests/ModelTests/PastryTests.cs
+++ BakeryBot.Tests/ModelTests/PastryTests.cs
@@ -12,41 +12,41 @@ namespace BakeryBot.Tests
     public void PastryCost_ReturnBaseCost_Int()
     {
       int pastryQuantity = 1;
-      double pastryCost = 5;
+      double pastryCost = 2;
       Pastry pastry = new Pastry(pastryQuantity);
       Assert.AreEqual(pastryCost, pastry.getPastryCost());
     }
-    [TestMethod]
-    public void PastryCost_ReturnCostForTwoPastry_Int()
-    {
-      int pastryQuantity = 2;
-      double pastryCost = 10;
-      Pastry pastry = new Pastry(pastryQuantity);
-      Assert.AreEqual(pastryCost, pastry.getPastryCost());
-    }
-    [TestMethod]
-    public void PastryCost_ReturnCostForThreePastry_Int()
-    {
-      int pastryQuantity = 3;
-      double pastryCost = 10;
-      Pastry pastry = new Pastry(pastryQuantity);
-      Assert.AreEqual(pastryCost, pastry.getPastryCost());
-    }
-    [TestMethod]
-    public void PastryCost_ReturnCostForTwelvePastry_Int()
-    {
-      int pastryQuantity = 12;
-      double pastryCost = 40;
-      Pastry pastry = new Pastry(pastryQuantity);
-      Assert.AreEqual(pastryCost, pastry.getPastryCost());
-    }
-    [TestMethod]
-    public void PastryCost_ReturnCostForNonPowerOfThreePastry_Int()
-    {
-      int pastryQuantity = 17;
-      double pastryCost = 70;
-      Pastry pastry = new Pastry(pastryQuantity);
-      Assert.AreEqual(pastryCost, pastry.getPastryCost());
-    }
+    // [TestMethod]
+    // public void PastryCost_ReturnCostForTwoPastry_Int()
+    // {
+    //   int pastryQuantity = 2;
+    //   double pastryCost = 10;
+    //   Pastry pastry = new Pastry(pastryQuantity);
+    //   Assert.AreEqual(pastryCost, pastry.getPastryCost());
+    // }
+    // [TestMethod]
+    // public void PastryCost_ReturnCostForThreePastry_Int()
+    // {
+    //   int pastryQuantity = 3;
+    //   double pastryCost = 10;
+    //   Pastry pastry = new Pastry(pastryQuantity);
+    //   Assert.AreEqual(pastryCost, pastry.getPastryCost());
+    // }
+    // [TestMethod]
+    // public void PastryCost_ReturnCostForTwelvePastry_Int()
+    // {
+    //   int pastryQuantity = 12;
+    //   double pastryCost = 40;
+    //   Pastry pastry = new Pastry(pastryQuantity);
+    //   Assert.AreEqual(pastryCost, pastry.getPastryCost());
+    // }
+    // [TestMethod]
+    // public void PastryCost_ReturnCostForNonPowerOfThreePastry_Int()
+    // {
+    //   int pastryQuantity = 17;
+    //   double pastryCost = 60;
+    //   Pastry pastry = new Pastry(pastryQuantity);
+    //   Assert.AreEqual(pastryCost, pastry.getPastryCost());
+    // }
   }
 }
--- BakeryBot/Models/Pastry.cs
+++ BakeryBot/Models/Pastry.cs
@@ -4,37 +4,48 @@ namespace BakeryBot.Models
 {
   public class Pastry
   {
-      private static double _PastryCost = 5;
-        private double _PastryDiscount;
-        public int PastryQuantity { get; set; }
+    private static double _PastryCost = 2;
+    private double _PastryDiscount;
+    public int PastryQuantity { get; set; }
 
-        public static double PastryCost
-        {
-            get { return _PastryCost; }
-            set { _PastryCost = value; }
-        }
-
-        public Pastry(int pastryQuantity)
-        {
-            _PastryCost = PastryCost;
-            _PastryDiscount = 5;
-            PastryQuantity = pastryQuantity;
-        }
-
-        public double getPastryCost()
-        {
-          double pastrySumCost = 0;
-          if (PastryQuantity < 3)
-          {
-            pastrySumCost = _PastryCost * PastryQuantity;
-          }
-          else
-          {
-            pastrySumCost = 
-            _PastryCost * PastryQuantity 
-            - (_PastryDiscount * (PastryQuantity / 3));
-          }
-          return pastrySumCost;
-        }
+    public static double PastryCost
+    {
+      get { return _PastryCost; }
+      set { _PastryCost = value; }
     }
+
+    public Pastry(int pastryQuantity)
+    {
+      _PastryCost = PastryCost;
+      _PastryDiscount = 5;
+      PastryQuantity = pastryQuantity;
+    }
+
+    public double getPastryCost()
+    {
+      double pastrySumCost = 0;
+      // if (PastryQuantity < 3)
+      // {
+      //   pastrySumCost = _PastryCost * PastryQuantity;
+      // }
+      if (PastryQuantity % 3 == 0)
+      {
+        pastrySumCost = _PastryCost * (PastryQuantity / 3);
+
+        // double numberOfDiscountedPastries = PastryQuantity / 3;
+
+        // pastrySumCost = _PastryCost * PastryQuantity - (_PastryDiscount * (PastryQuantity / 3));
+      }
+      // else if (something)
+      // {
+      //   double numberOfDiscountedPastries = PastryQuantity / 3;
+      //   // ...?
+      // }
+      else
+      {
+        pastrySumCost = _PastryCost * PastryQuantity;
+      }
+      return pastrySumCost;
+    }
+  }
 }
