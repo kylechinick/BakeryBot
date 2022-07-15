9cb1068c6761c7b445d67b91a2739f88a8099d7c
Kyle Chinick <kylechinick@icloud.com>
2022-07-15 15:03:35 -0700

    Replace commented contents with boilerplate Pastry version from Bread.cs

--- BakeryBot/Models/Pastry.cs
+++ BakeryBot/Models/Pastry.cs
@@ -1,7 +1,40 @@
-// namespace BakeryBot.Models.Pastry
-// {
-//   public class Pastry
-//   {
-//     // properties, constructors, methods, etc. go here
-//   }
-// }
+using System;
+
+namespace BakeryBot.Models
+{
+  public class Pastry
+  {
+      private static double _PastryCost = 5;
+        private double _PastryDiscount;
+        public int PastryQuantity { get; set; }
+
+        public static double PastryCost
+        {
+            get { return _PastryCost; }
+            set { _PastryCost = value; }
+        }
+
+        public Pastry(int pastryQuantity)
+        {
+            _PastryCost = PastryCost;
+            _PastryDiscount = 5;
+            PastryQuantity = pastryQuantity;
+        }
+
+        public double getPastryCost()
+        {
+          double pastrySumCost = 0;
+          if (PastryQuantity < 3)
+          {
+            pastrySumCost = _PastryCost * PastryQuantity;
+          }
+          else
+          {
+            pastrySumCost = 
+            _PastryCost * PastryQuantity 
+            - (_PastryDiscount * (PastryQuantity / 3));
+          }
+          return pastrySumCost;
+        }
+    }
+}
