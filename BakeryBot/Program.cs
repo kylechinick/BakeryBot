6b78c272af6b37a999ee53a90d0fe2a09274589f
Kyle Chinick <kylechinick@icloud.com>
2022-07-15 14:42:54 -0700

    Fix Program.cs visibility by uncommenting entry point ref and moving file into main project directory

--- BakeryBot/Program.cs
+++ BakeryBot/Program.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using BakeryBot.Models.Bread;
-using BakeryBot.Models.Pastry;
+using BakeryBot.Models;
+// using BakeryBot.Models.Bread;
+// using BakeryBot.Models.Pastry;
 
 namespace BakeryBot
 {
