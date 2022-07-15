4df7bdd041eb0c77cd88aa5034db33a73f7d9e08
Kyle Chinick <kylechinick@icloud.com>
2022-07-15 16:22:02 -0700

    Update formatting to reduct white space

--- BakeryBot/Models/Bread.cs
+++ BakeryBot/Models/Bread.cs
@@ -4,43 +4,41 @@ namespace BakeryBot.Models
 {
   public class Bread
   {
-      private static double _BreadCost = 5;
-        private double _BreadDiscount;
-        public int BreadQuantity { get; set; }
+    private static double _BreadCost = 5;
+    private double _BreadDiscount;
+    public int BreadQuantity { get; set; }
 
-        public static double BreadCost
-        {
-            get { return _BreadCost; }
-            set { _BreadCost = value; }
-        }
-
-        public Bread(int breadQuantity)
-        {
-            _BreadCost = BreadCost;
-            _BreadDiscount = 5;
-            BreadQuantity = breadQuantity;
-        }
-
-        public double getBreadCost()
-        {
-          double breadSumCost = 0;
-          if (BreadQuantity < 3)
-          {
-            breadSumCost = _BreadCost * BreadQuantity;
-          }
-          // else if (BreadQuantity % 3 == 0)
-          // {
-          //   breadSumCost = 
-          //   _BreadCost * BreadQuantity 
-          //   - (_BreadDiscount * (BreadQuantity / 3));
-          // }
-          else
-          {
-            breadSumCost = 
-            _BreadCost * BreadQuantity 
-            - (_BreadDiscount * (BreadQuantity / 3));
-          }
-          return breadSumCost;
-        }
+    public static double BreadCost
+    {
+      get { return _BreadCost; }
+      set { _BreadCost = value; }
     }
+
+    public Bread(int breadQuantity)
+    {
+      _BreadCost = BreadCost;
+      _BreadDiscount = 5;
+      BreadQuantity = breadQuantity;
+    }
+
+    public double getBreadCost()
+    {
+      double breadSumCost = 0;
+      if (BreadQuantity < 3)
+      {
+        breadSumCost = _BreadCost * BreadQuantity;
+      }
+      // else if (BreadQuantity % 3 == 0)
+      // {
+      //   breadSumCost = 
+      //   _BreadCost * BreadQuantity 
+      //   - (_BreadDiscount * (BreadQuantity / 3));
+      // }
+      else
+      {
+        breadSumCost = _BreadCost * BreadQuantity - (_BreadDiscount * (BreadQuantity / 3));
+      }
+      return breadSumCost;
+    }
+  }
 }
